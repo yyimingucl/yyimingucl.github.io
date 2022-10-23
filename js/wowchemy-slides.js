83a9a2230d4936311e45b04e2712ad0734141161
yyimingucl <yyimingucl@users.noreply.github.com>
2022-10-23 18:00:59 +0000

    deploy: a1150a6a223b0fa6eebf52e368c18d123236ce29

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -1,6 +1,6 @@
 (() => {
   // ns-params:@params
-  var slides = { highlight_style: "dracula", theme: "black" };
+  var slides = null;
 
   // <stdin>
   var enabledPlugins = [RevealMarkdown, RevealSearch, RevealNotes, RevealMath.KaTeX, RevealZoom];
